80bfd53b2ed52dff8bd83909c5b35c51146248d5
Yohann <73314529+yohann69@users.noreply.github.com>
2024-03-06 16:58:12 +0100

    Better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,39 +3,31 @@ import {Gemini} from "@/pages/Gemini";
 import {TracingBeam} from "@/components/ui/tracing-beam";
 import React from "react";
 import {BackgroundGradientAnimation} from "@/components/ui/background-gradient-animation";
-import {EvervaultCard, Icon} from "@/components/ui/evervault-card";
 import {Sparkles} from "@/pages/Particules";
 import {NavBar} from "@/pages/NavBar";
 import {Cards} from "@/pages/Cards";
 import {Pictures} from "@/pages/Pictures";
+import {MultiTabs} from "@/pages/MultiTabs";
+import {CodeCard} from "@/pages/CodeCard";
+import {WaveBackground} from "@/pages/WaveBackground";
 
 export default function Home() {
 	return (
 		<>
 			<NavBar/>
-			<Sparkles/>
+			<WaveBackground/>
 			<Cards/>
 			<Pictures/>
+			<Sparkles/>
+			
 			<TracingBeam>
+
+				<MultiTabs/>
+
 				<MacBook/>
 				<Gemini/>
-				<div
-					className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
-					<Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black"/>
-					<Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black"/>
-					<Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black"/>
-					<Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black"/>
 
-					<EvervaultCard text="GEggzeg ezg ezgezgze gezgezgzegze"/>
-
-					<h2 className="dark:text-white text-black mt-4 text-sm font-light">
-						Hover over this card to reveal an awesome effect. Running out of copy
-						here.
-					</h2>
-					<p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
-						Watch me hover
-					</p>
-				</div>
+				<CodeCard/>
 			</TracingBeam>
 
 			<BackgroundGradientAnimation>
