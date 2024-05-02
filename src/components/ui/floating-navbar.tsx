72b1ef9c618f363314461cef9c48bbf230c95cf3
Yohann <73314529+yohann69@users.noreply.github.com>
2024-05-02 12:03:28 +0200

    better

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -70,7 +70,7 @@ export const FloatingNav = ({
                 <Link href="#contact">
                     <button
                         className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
-                        <span>Conact</span>
+                        <span>Contact</span>
                         <span
                             className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px"/>
                     </button>
