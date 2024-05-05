5333ac3feb2469d5479b88c2c0635f13a5d69aaf
Yohann <73314529+yohann69@users.noreply.github.com>
2024-05-05 17:07:54 +0200

    Fixing the nasty bug

--- src/pages/LangagesOutils.tsx
+++ src/pages/LangagesOutils.tsx
@@ -18,82 +18,82 @@ export default function LangagesOutils() {
                     <a href="https://developer.mozilla.org/en-US/docs/Web/javascript" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                        src="/javascript-original.svg"
                         alt="JavaScript"/></a>
                     <a href="https://nodejs.org/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-plain.svg"
+                        src="/nodejs-plain.svg"
                         alt="NodeJS"/></a>
                     <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"
+                        src="/html5-original.svg"
                         alt="html"/></a>
                     <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"
+                        src="/css3-original.svg"
                         alt="CSS"/></a>
                     <a href="https://www.python.org/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+                        src="/python-original.svg"
                         alt="Python"/></a>
                     <a href="https://www.php.net/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"
+                        src="/php-original.svg"
                         alt="PHP"/></a>
                     <a href="https://www.java.com/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
+                        src="/java-original.svg"
                         alt="Java"/></a>
                     <a href="https://en.wikipedia.org/wiki/C_(programming_language)" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
+                        src="/c-original.svg"
                         alt="C"/></a>
                     <a href="https://www.mysql.com/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg"
+                        src="/mysql-original.svg"
                         alt="MYSQL"/></a>
                     <a href="https://www.postgresql.org/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg"
+                        src="/Postgresql_elephant.svg"
                         alt="Postgresql"/></a>
                     <a href="https://www.mongodb.com/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg"
+                        src="/mongodb-original.svg"
                         alt="MongoDB"/></a>
                     <a href="https://go.dev/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg"
+                        src="/go-original.svg"
                         alt="Golang"/></a>
                     <a href="https://godotengine.org/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/godot/godot-original.svg"
+                        src="/godot-original.svg"
                         alt="Godot"/></a>
                     <a href="https://reactnative.dev/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://reactnative.dev/img/header_logo.svg"
+                        src="/header_logo.svg"
                         alt="React Native"/></a>
                     <a href="https://www.solidjs.com/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/solidjs/solidjs-original.svg"
+                        src="/solidjs-original.svg"
                         alt="SolidJS"/></a>
                     <a href="https://www.rust-lang.org/" target="_blank"><Image
                         width={200}
                         height={200}
-                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/rust/rust-original.svg"
+                        src="/rust-original.svg"
                         alt="Rust"/></a>
                 </div>
             </div>
@@ -106,22 +106,22 @@ export default function LangagesOutils() {
                         <a href="https://www.jetbrains.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/jetbrains/jetbrains-original.svg"
+                            src="/jetbrains-original.svg"
                             alt="Jetbrains"/></a>
                         <a href="https://code.visualstudio.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg"
+                            src="/vscode-original.svg"
                             alt="Visual Studio Code"/></a>
                         <a href="https://www.office.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Microsoft_365_%282022%29.svg"
+                            src="/Microsoft_365_%282022%29.svg"
                             alt="Office 365"/></a>
                         <a href="https://www.adobe.com/creativecloud.html" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Adobe_Creative_Cloud_rainbow_icon.svg"
+                            src="/Adobe_Creative_Cloud_rainbow_icon.svg"
                             alt="Creative Cloud"/></a>
                     </div>
 
@@ -132,22 +132,22 @@ export default function LangagesOutils() {
                         <a href="https://www.canva.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/canva/canva-original.svg"
+                            src="/canva-original.svg"
                             alt="Canva"/></a>
                         <a href="https://www.figma.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg"
+                            src="/figma-original.svg"
                             alt="Figma"/></a>
                         <a href="https://trello.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/trello/trello-plain.svg"
+                            src="/trello-plain.svg"
                             alt="Trello"/></a>
                         <a href="https://git-scm.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg"
+                            src="/git-original.svg"
                             alt="Git"/></a>
                     </div>
                 </div>
@@ -158,22 +158,22 @@ export default function LangagesOutils() {
                         <a href="https://windows.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://upload.wikimedia.org/wikipedia/commons/8/87/Windows_logo_-_2021.svg"
+                            src="/Windows_logo_-_2021.svg"
                             alt="Windows 10/11"/></a>
                         <a href="https://www.debian.org/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/debian/debian-original.svg"
+                            src="/debian-original.svg"
                             alt="Debian"/></a>
                         <a href="https://getfedora.org/fr/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/fedora/fedora-original.svg"
+                            src="/fedora-original.svg"
                             alt="Fedora"/></a>
                         <a href="https://ubuntu.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/ubuntu/ubuntu-plain.svg"
+                            src="/ubuntu-plain.svg"
                             alt="Ubuntu"/></a>
                     </div>
 
@@ -184,21 +184,21 @@ export default function LangagesOutils() {
                         <a href="https://en.wikipedia.org/wiki/Bash_(Unix_shell)" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://bashlogo.com/img/symbol/svg/full_colored_light.svg" alt="Bash"/></a>
+                            src="/full_colored_light.svg" alt="Bash"/></a>
                         <a href="https://cmake.org/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cmake/cmake-original.svg"
+                            src="/cmake-original.svg"
                             alt="Cmake"/></a>
                         <a href="https://eslint.org/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/eslint/eslint-original.svg"
+                            src="/eslint-original.svg"
                             alt="ESLint"/></a>
                         <a href="https://www.npmjs.com/" target="_blank"><Image
                             width={200}
                             height={200}
-                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/npm/npm-original-wordmark.svg"
+                            src="/npm-original-wordmark.svg"
                             alt="NPM"/></a>
                     </div>
                 </div>
